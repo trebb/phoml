0eeb36390613e0ea2ce0251b929c429ce9dd5d8a
stesch <stesch@9521289c-3fb7-4341-aae7-02de4671f267>
2008-10-30 12:45:12 +0000

    Neue Klasse zum Testen des Vorwärtsschnittes true-> hat alles geklappt
    git-svn-id: http://www/repos/lib/photogrammetrie/trunk@761 9521289c-3fb7-4341-aae7-02de4671f267

---
+++ wrapper_for_java/CPhotogrammetrieTest.h
@@ -0,0 +1,14 @@
+#ifndef CPHOTOGRAMMETRIETEST_H_
+#define CPHOTOGRAMMETRIETEST_H_
+
+class CPhotogrammetrieTest
+{
+public:
+	CPhotogrammetrieTest();
+	virtual ~CPhotogrammetrieTest();
+	
+	bool check();
+	
+};
+
+#endif /*CPHOTOGRAMMETRIETEST_H_*/
