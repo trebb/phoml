ff34f24ee712c8d5c3611771d4efbae33124617b
dirk <dirk@9521289c-3fb7-4341-aae7-02de4671f267>
2009-12-02 09:33:35 +0000

    git-svn-id: http://www/repos/lib/photogrammetrie/trunk@965 9521289c-3fb7-4341-aae7-02de4671f267

--- wrapper_for_java/mainwrapperjava.cpp
+++ wrapper_for_java/mainwrapperjava.cpp
@@ -18,12 +18,12 @@ Point CMainWrapperJava::get_3D_Point_local(CBPointList BPlist)
 {
 	Vorwaertsschnitt FWS(BPlist.m_BPoint_List);
 
-	return FWS.get_Schnittpunkt().DivS(1000.0);	
+	return FWS.get_Schnittpunkt().DivS(1000.0);
 }
 
 BPoint CMainWrapperJava::get_BPoint_from_local_3D_Point(Point P_local,CCam_bore cam)
 {
-  return BPoint(cam,P_local.get_X()*1000.0,P_local.get_Y()*1000.0,P_local.get_Z()*1000.0);   	
+  return BPoint(cam,P_local.get_X()*1000.0,P_local.get_Y()*1000.0,P_local.get_Z()*1000.0);
 }
 
 Point CMainWrapperJava::get_3D_Point_global(Point P_local_in_m,CCam_bore cam, double E, double N, double eH, double roll, double pitch, double heading)
@@ -31,16 +31,16 @@ Point CMainWrapperJava::get_3D_Point_global(Point P_local_in_m,CCam_bore cam, do
 	CBoreside_transformation bore(cam);
 
 	double mroll=0.0,mpitch=0.0,mheading=0.0;
-	
+
 	CApplanix appl;
 	//appl.calc_approximately_meridian_convergence_degree(E,latitude,Hea);
 	appl.convert_angles_UTM_to_math_coo_system(roll,pitch,heading,mroll,mpitch,mheading);
 
 	//bore.set_car_position_utm(E,N,eH,rroll,rpitch,rheading);
 	bore.set_car_position_utm(E,N,eH,roll,pitch,heading);
-		
+
 	bore.set_local_koordinate(P_local_in_m.get_X(),P_local_in_m.get_Y(),P_local_in_m.get_Z());
-	
+
  return bore.get_utm_koordinate();
 }
 
@@ -54,12 +54,12 @@ Point CMainWrapperJava::get_3D_Point_local(Point P_global_E_N_eH_in_m,CCam_bore 
 	//appl.calc_approximately_meridian_convergence_degree(E,latitude,Hea);
 	appl.convert_angles_UTM_to_math_coo_system(roll,pitch,heading,mroll,mpitch,mheading);
 
-	
+
 	//bore.set_car_position_utm(E,N,eH,rroll,rpitch,rheading);
 	bore.set_car_position_utm(E,N,eH,roll,pitch,heading);
-	
+
 	bore.set_utm_koordinate(P_global_E_N_eH_in_m.get_X(),P_global_E_N_eH_in_m.get_Y(),P_global_E_N_eH_in_m.get_Z());
-	
+
 	return bore.get_local_koordinate();
 }
 
@@ -72,7 +72,7 @@ Point CMainWrapperJava::get_3D_Point_global_wMC(Point P_local_in_m,CCam_bore cam
 	double mroll=0.0,mpitch=0.0,mheading=0.0;
 
 	CApplanix appl;
-	appl.calc_approximately_meridian_convergence_degree(E,latitude,eH);
+	appl.calc_approximately_meridian_convergence_degree(E,latitude,heading);
 	appl.convert_angles_UTM_to_math_coo_system(roll,pitch,heading,mroll,mpitch,mheading);
 
 	bore.set_car_position_utm(E,N,eH,roll,pitch,heading);
@@ -98,4 +98,4 @@ Point CMainWrapperJava::get_3D_Point_local_wMC(Point P_global_E_N_eH_in_m,CCam_b
 
 	return bore.get_local_koordinate();
 }
-	
+
