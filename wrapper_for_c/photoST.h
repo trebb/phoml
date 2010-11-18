262a170f6e840da99168b3e2c0ea3365fc0b5ee0
Bert Burgemeister <trebbu@googlemail.com>
2010-11-18 13:47:07 +0100

    Some untested refactoring.

--- wrapper_for_c/photoST.h
+++ wrapper_for_c/photoST.h
@@ -59,14 +59,14 @@ extern "C" {
   _DLL_EXPORT double STDCALL get_latitude();
   _DLL_EXPORT double STDCALL get_longitude();
 
-  _DLL_EXPORT int STDCALL    addCam(int pix_col,int pix_row,double pix_size,double dx,double dy,double dz,double omega,double phi,double kappa,double c,double xh,double yh,double A1,double A2,double A3,double B1,double B2,double C1,double C2,double R0,double B_dx,double B_dy,double B_dz,double B_ddx,double B_ddy,double B_ddz,double B_rotx,double B_roty,double B_rotz,double B_drotx,double B_droty,double B_drotz);
+  _DLL_EXPORT int STDCALL    addCam(int sensor_width_pix,int sensor_height_pix,double pix_size,double dx,double dy,double dz,double omega,double phi,double kappa,double c,double xh,double yh,double A1,double A2,double A3,double B1,double B2,double C1,double C2,double R0,double B_dx,double B_dy,double B_dz,double B_ddx,double B_ddy,double B_ddz,double B_rotx,double B_roty,double B_rotz,double B_drotx,double B_droty,double B_drotz);
   _DLL_EXPORT int STDCALL    addCam2(char* ini_file);
   _DLL_EXPORT int STDCALL    addBPoint(double m,double n);
-  _DLL_EXPORT int STDCALL    addBPoint2(int pix_col,int pix_row,double pix_size,double dx,double dy,double dz,double omega,double phi,double kappa,double c,double xh,double yh,double A1,double A2,double A3,double B1,double B2,double C1,double C2,double R0,double B_dx,double B_dy,double B_dz,double B_ddx,double B_ddy,double B_ddz,double B_rotx,double B_roty,double B_rotz,double B_drotx,double B_droty,double B_drotz,double m,double n);
-  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint(double Easting, double Northing, double eHeigth, double roll, double pitch, double heading, double latitude, double longitude);
-  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint_CamSetGlobal(double Easting, double Northing, double eHeigth, double roll, double pitch, double heading, double latitude, double longitude);
-  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint_std(double dEasting, double dNorthing, double deHeigth, double droll, double dpitch, double dheading);
-  _DLL_EXPORT int STDCALL    addGlobalMeasurementPoint(double Easting, double Northing, double eHeigth);
+  _DLL_EXPORT int STDCALL    addBPoint2(int sensor_width_pix,int sensor_height_pix,double pix_size,double dx,double dy,double dz,double omega,double phi,double kappa,double c,double xh,double yh,double A1,double A2,double A3,double B1,double B2,double C1,double C2,double R0,double B_dx,double B_dy,double B_dz,double B_ddx,double B_ddy,double B_ddz,double B_rotx,double B_roty,double B_rotz,double B_drotx,double B_droty,double B_drotz,double m,double n);
+  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint(double Easting, double Northing, double Height, double roll, double pitch, double heading, double latitude, double longitude);
+  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint_CamSetGlobal(double Easting, double Northing, double Height, double roll, double pitch, double heading, double latitude, double longitude);
+  _DLL_EXPORT void STDCALL   addGlobalCarReferencePoint_std(double easting_sd, double northing_sd, double heigth_sd, double roll_sd, double pitch_sd, double heading_sd);
+  _DLL_EXPORT int STDCALL    addGlobalMeasurementPoint(double Easting, double Northing, double Height);
   _DLL_EXPORT int STDCALL    addLocalMeasurementPoint(double X, double Y, double Z);
   _DLL_EXPORT int STDCALL    addRefGroundSurface(double nx, double ny, double nz, double d);
   _DLL_EXPORT void STDCALL   setGlobalReferenceFrame();
